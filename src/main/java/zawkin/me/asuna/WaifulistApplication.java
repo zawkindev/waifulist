84a27018e838251b30c4bd318909014a0630a738
zawkindev <zawkindev@gmail.com>
2024-11-29 23:49:02 +0500

    initial commit

---
+++ src/main/java/zawkin/me/asuna/WaifulistApplication.java
@@ -0,0 +1,13 @@
+package zawkin.me.asuna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaifulistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaifulistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/zawkin/me/asuna/WaifulistApplicationTests.java
@@ -0,0 +1,13 @@
+package zawkin.me.asuna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaifulistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
